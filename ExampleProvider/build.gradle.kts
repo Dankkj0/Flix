5d2559a164542dc322ed0b7ffc8926a85b55a327
Hridoy Matber <hridoy200065@diit.edu.bd>
2026-05-29 01:49:07 +0600

    Update build.gradle.kts

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -5,8 +5,9 @@ buildscript {
         maven("https://jitpack.io")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.2.0") // আপডেট করা হলো
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22") // আপডেট করা হলো
+        classpath("com.android.tools.build:gradle:8.2.0")
+        // এখানে লেটেস্ট কটলিন ভার্সন দেওয়া হলো
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24") 
     }
 }
 
