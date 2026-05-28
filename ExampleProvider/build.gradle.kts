68f91a56c4f148adcf0471148aba5a3eba91ce89
Hridoy Matber <hridoy200065@diit.edu.bd>
2026-05-28 09:18:29 +0600

    Update build.gradle.kts

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -1,38 +1,12 @@
-dependencies {
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
-}
+import com.lagradost.cloudstream3.gradle.CloudstreamExtension
 
-// Use an integer for version numbers
-version = 1
+val pluginConfig = extensions.getByType<CloudstreamExtension>()
 
-cloudstream {
-    // All of these properties are optional, you can safely remove any of them.
-
-    description = "Lorem ipsum"
-    authors = listOf("Cloudburst", "Luna712")
-
-    /**
-    * Status int as one of the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta-only
-    **/
-    status = 1 // Will be 3 if unspecified
-
-    tvTypes = listOf("Movie")
-
-    requiresResources = true
-    language = "en"
-
-    // Random CC logo I found
-    iconUrl = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Korduene_Logo.png"
-}
-
-android {
-    buildFeatures {
-        buildConfig = true
-        viewBinding = true
-    }
+pluginConfig.apply {
+    pluginId = "StreamBDPlugin"
+    pluginName = "StreamBD IPTV"
+    // এখানে প্যাকেজের নাম ঠিক করে দেওয়া হয়েছে
+    pluginClass = "com.iptv.streambd.StreamBDPlugin" 
+    pluginVersion = 1
+    description = "StreamBD IPTV Extension with Token Parser"
 }
