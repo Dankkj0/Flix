b599c60df616dd43c3bb5fac0e324671fdb3bb6e
Hridoy Matber <hridoy200065@diit.edu.bd>
2026-05-28 10:12:56 +0600

    Update build.gradle.kts

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -1,11 +1,7 @@
-import com.lagradost.cloudstream3.gradle.CloudstreamExtension
-
-val pluginConfig = extensions.getByType<CloudstreamExtension>()
-
-pluginConfig.apply {
-    this.pluginId = "StreamBDPlugin"
-    this.pluginName = "StreamBD IPTV"
-    this.pluginClass = "com.iptv.streambd.StreamBDPlugin" 
-    this.pluginVersion = 1
-    this.description = "StreamBD IPTV Extension with Token Parser"
+cloudstream {
+    setPluginId("StreamBDPlugin")
+    setPluginName("StreamBD IPTV")
+    setPluginClass("com.iptv.streambd.StreamBDPlugin")
+    setPluginVersion(1)
+    setDescription("StreamBD IPTV Extension with Token Parser")
 }
