4236c70eb9a2fb228b1a8912f582e109572db3fb
tydbq <97149776+tydbq@users.noreply.github.com>
2026-05-31 14:19:49 +0530

    Refactor build.gradle.kts for Android library setup
    Removed old dependencies and cloudstream configuration. Updated Android SDK version and added new dependencies.

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -1,38 +1,15 @@
-dependencies {
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
-}
-
-// Use an integer for version numbers
-version = 1
-
-cloudstream {
-    // All of these properties are optional, you can safely remove any of them.
-
-    description = "Lorem ipsum"
-    authors = listOf("Cloudburst", "Luna712")
-
-    /**
-    * Status int as one of the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta-only
-    **/
-    status = 1 // Will be 3 if unspecified
-
-    tvTypes = listOf("Movie")
-
-    requiresResources = true
-    language = "en"
-
-    // Random CC logo I found
-    iconUrl = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Korduene_Logo.png"
+plugins {
+    id("com.android.library")
+    kotlin("android")
 }
 
 android {
-    buildFeatures {
-        buildConfig = true
-        viewBinding = true
+    compileSdk = 33
+    defaultConfig {
+        minSdk = 21
     }
+}
+
+dependencies {
+    implementation("com.github.recloudstream:cloudstream3:master-SNAPSHOT")
 }
