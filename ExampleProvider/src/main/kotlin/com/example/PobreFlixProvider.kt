22b78d0093e83c0c6e76ffb42301a2b351e9fb4c
dankkj <mailatorresneh@gmail.com>
2026-07-11 12:04:22 -0300

    Import loadExtractor function from ExtractorApiKt

--- ExampleProvider/src/main/kotlin/com/example/PobreFlixProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/PobreFlixProvider.kt
@@ -2,6 +2,7 @@ package com.example // Altere para o package correto do seu projeto
 
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
+import com.lagradost.cloudstream3.utils.ExtractorApiKt.loadExtractor
 import org.jsoup.nodes.Element
 
 class PobreFlixProvider : MainAPI() {
